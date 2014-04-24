f2183c05f01b4997618ce23cc597e4508230b9ff
Bellonor <bellonor@126.com>
2014-04-24 11:16:11 +0800

    第七周Paoding搭建好，简单的分词HELLO world的提交

---
+++ com.homework/src/week7/classfier/PaodingTest.java
@@ -0,0 +1,28 @@
+package classfier;
+
+import java.io.StringReader;
+
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import net.paoding.analysis.analyzer.PaodingAnalyzer;
+
+public class PaodingTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String line="中华人民共和国";
+        PaodingAnalyzer analyzer=new PaodingAnalyzer();
+        StringReader sr=new StringReader(line);
+        TokenStream ts=analyzer.tokenStream("", sr);
+        try{
+        	while(ts.incrementToken()){
+        		CharTermAttribute ta=ts.getAttribute(CharTermAttribute.class);
+        		System.out.println(ta.toString());
+        	}
+        }catch(Exception e){
+        	
+        }
+	}
+
+}
