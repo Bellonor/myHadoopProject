c7c222d6737fc69bff503410253c391b0055d1e5
Bellonor <bellonor@126.com>
2014-06-05 18:21:04 +0800

    带USERID字段的K-means算法实现

---
+++ com.homework/src/sequence/machineleaning/clustering/mysequence/machineleaning/clustering/canopy/UserPoint.java
@@ -0,0 +1,35 @@
+package mysequence.machineleaning.clustering.canopy;
+
+public class UserPoint {
+
+	private Double x;
+	private Double y;
+	private Integer sign=-1;
+	private String userid;
+	
+	public Double getX() {
+		return x;
+	}
+	public void setX(Double x) {
+		this.x = x;
+	}
+	public Double getY() {
+		return y;
+	}
+	public void setY(Double y) {
+		this.y = y;
+	}
+	public Integer getSign() {
+		return sign;
+	}
+	public void setSign(Integer sign) {
+		this.sign = sign;
+	}
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	
+}
