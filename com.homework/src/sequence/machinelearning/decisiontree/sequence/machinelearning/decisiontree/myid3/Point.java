21aea3737ec34f312b09bee73d5b4c785154bc12
Bellonor <bellonor@126.com>
2014-06-24 18:44:24 +0800

    dd

---
+++ com.homework/src/sequence/machinelearning/decisiontree/sequence/machinelearning/decisiontree/myid3/Point.java
@@ -0,0 +1,21 @@
+package sequence.machinelearning.decisiontree.myid3;
+
+public class Point {
+
+	private Double sv;
+	private Double entropySv;
+	public Double getSv() {
+		return sv;
+	}
+	public void setSv(Double sv) {
+		this.sv = sv;
+	}
+	public Double getEntropySv() {
+		return entropySv;
+	}
+	public void setEntropySv(Double entropySv) {
+		this.entropySv = entropySv;
+	}
+	
+	
+}
