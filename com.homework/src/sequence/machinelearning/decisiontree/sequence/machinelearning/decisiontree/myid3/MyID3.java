f483bf0a2fca6fb521524791249506925bcc4ab7
Bellonor <bellonor@126.com>
2014-06-24 18:43:15 +0800

    完成了第一个gain的计算

--- com.homework/src/sequence/machinelearning/decisiontree/sequence/machinelearning/decisiontree/myid3/MyID3.java
+++ com.homework/src/sequence/machinelearning/decisiontree/sequence/machinelearning/decisiontree/myid3/MyID3.java
@@ -6,21 +6,97 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.LinkedList;
 
 public class MyID3 {
 
-    private LinkedList<String> attribute = new LinkedList<String>(); // 存储属性的名称
-    private LinkedList<ArrayList<String>> attributevalue = new LinkedList<ArrayList<String>>(); // 存储每个属性的取值
-    private LinkedList<String[]> data = new LinkedList<String[]>();; // 原始数据
+    private static LinkedList<String> attribute = new LinkedList<String>(); // 存储属性的名称
+    private static LinkedList<ArrayList<String>> attributevalue = new LinkedList<ArrayList<String>>(); // 存储每个属性的取值
+    private static LinkedList<String[]> data = new LinkedList<String[]>();; // 原始数据
    
     public static final String patternString = "@attribute(.*)[{](.*?)[}]";
+	public static String[] yesNo;
+	public static TreeNode root;
 	
+
+    public void createDTree(){
+    	root=new TreeNode();
+    	
+    }
+    /**
+     * 
+     * @param lines 传入要分析的数据集
+     * @param index 哪个属性？attribute的index
+     */
+    public Double fisrtGain(LinkedList<String[]> lines,int index){
+    	Double gain=-1.0;
+    	List<Double> li=new ArrayList<Double>();
+    	//统计Yes No的次数
+    	for(int i=0;i<yesNo.length;i++){
+    		Double sum=0.0;
+    		for(int j=0;j<lines.size();j++){
+    			String[] line=lines.get(j);
+    			//data为结构化数据,如果数据最后一列==yes,sum+1
+    			if(line[line.length-1].equals(yesNo[i])){
+    				sum=sum+1;
+    			}
+    		}
+    		li.add(sum);
+    	}
+    	//计算Entropy(S)计算Entropy(S) 见参考书《机器学习 》Tom.Mitchell著  第3.4.1.2节
+    	Double entropyS=TheMath.getEntropy(lines.size(), li);
+    	//下面计算gain
+    	
+    	List<String> la=attributevalue.get(index);
+    	List<Point> lasv=new ArrayList<Point>();
+    	for(int n=0;n<la.size();n++){
+    		String attvalue=la.get(n);
+        	//统计Yes No的次数
+    		List<Double> lisub=new ArrayList<Double>();//如：sunny 是yes时发生的次数，是no发生的次数
+    		Double Sv=0.0;//公式3.4中的Sv 见参考书《机器学习(Tom.Mitchell著)》
+        	for(int i=0;i<yesNo.length;i++){
+        		Double sum=0.0;
+        		for(int j=0;j<lines.size();j++){
+        			String[] line=lines.get(j);
+        			//data为结构化数据,如果数据最后一列==yes,sum+1
+        			if(line[index].equals(attvalue)&&line[line.length-1].equals(yesNo[i])){
+        				sum=sum+1;
+        			}
+        		}
+        		Sv=Sv+sum;//计算总数
+        		lisub.add(sum);
+        	}
+        	//计算Entropy(S) 见参考书《机器学习(Tom.Mitchell著)》
+        	Double entropySv=TheMath.getEntropy(Sv.intValue(), lisub);
+        	//
+        	Point p=new Point();
+        	p.setSv(Sv);
+        	p.setEntropySv(entropySv);
+        	lasv.add(p);
+    	}
+    	//gain=TheMath.getGain(entropyS,lines.size(),lasv);
+    	return gain;
+    }
+    public void insertNode(){
+    	
+    }
+    public void getMaxGain(){
+    	
+    }
+    
+    public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    	MyID3 myid3 = new MyID3();
+    	myid3.readARFF(new File("datafile/decisiontree/test/in/weather.nominal.arff"));
+    	myid3.fisrtGain(data,0);
+	}
     //读取arff文件，给attribute、attributevalue、data赋值
     public void readARFF(File file) {
         try {
@@ -29,7 +105,13 @@ public class MyID3 {
             String line;
             Pattern pattern = Pattern.compile(patternString);
             while ((line = br.readLine()) != null) {
-                Matcher matcher = pattern.matcher(line);
+            	if (line.startsWith("@decision")) {
+                   line = br.readLine();
+                        if(line=="")
+                            continue;
+                        yesNo = line.split(",");
+                }
+            	Matcher matcher = pattern.matcher(line);
                 if (matcher.find()) {
                     attribute.add(matcher.group(1).trim());
                     String[] values = matcher.group(2).split(",");
@@ -55,10 +137,5 @@ public class MyID3 {
         }
     }
 	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
 
 }
--- com.homework/src/sequence/machinelearning/decisiontree/sequence/machinelearning/decisiontree/myid3/TheMath.java
+++ com.homework/src/sequence/machinelearning/decisiontree/sequence/machinelearning/decisiontree/myid3/TheMath.java
@@ -14,29 +14,30 @@ public class TheMath {
 	 * @param li 各个事件发生的次数
 	 * @return
 	 */
-	public static  Double getEntropy(Double S,List<Double> li){
-		Double entropy=new Double(-1.0);
+	public static  Double getEntropy(Integer S,List<Double> li){
+		
+		Double entropy=new Double(0.0);
 		for(int i=0;i<li.size();i++){
-			entropy=entropy+sigma(li.get(i),S);
+			entropy=entropy+sigma(li.get(i),Double.valueOf(S));
 		}
 		return entropy;
 	}
 	//信息增益公式
 	/**
-	 * 
+	 * @param 此公式完全参照  《机器学习(Tom.Mitchell著)》3.4节
 	 * @param entropyS S的信息熵
-	 * @param total 传入的总数
+	 * @param S 传入的总数
 	 * @param sv sv.key 是sv的个数,sv.value是entropy(sv)
-	 * @return
+	 * @return 返回信息增益
 	 */
-	public static Double getGain(Double entropyS,Double total,Map<Double,Double> sv){
-		Double gain=new Double(-1.0);
+	public static Double getGain(Double entropyS,int S,Map<Double,Double> sv){
+		Double gain=new Double(0.0);
 		Double enSum=new Double(0.0);
 		Map.Entry<Double, Double>entry;
 		Iterator<Entry<Double,Double>> iter=sv.entrySet().iterator();
 		while(iter.hasNext()){
 			entry=iter.next();
-			enSum=enSum+((entry.getKey()/total)*entry.getValue());
+			enSum=enSum+((entry.getKey()/Double.valueOf(S))*entry.getValue());
 		}
 		gain=entropyS-enSum;
 		return gain;
